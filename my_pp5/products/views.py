06626e43a42f692c1182541f4a3abf251e5ab1a3
Richard Goggin <richardgoggin@gmail.com>
2024-12-10 22:44:36 +0000

    Added search functionality to product catalog

--- my_pp5/products/views.py
+++ my_pp5/products/views.py
@@ -2,17 +2,22 @@ from django.shortcuts import render
 from .models import Product, Category
 
 def product_catalog(request):
-    """View to display all products with category filtering."""
+    """View to display all products with category filtering and search."""
+    query = request.GET.get('q', None)
     category_name = request.GET.get('category', None)
     categories = Category.objects.all()
 
+    products = Product.objects.all()
+
     if category_name:
-        products = Product.objects.filter(category__name=category_name)
-    else:
-        products = Product.objects.all()
+        products = products.filter(category__name=category_name)
+
+    if query:
+        products = products.filter(name__icontains=query)
 
     return render(request, 'products/catalog.html', {
         'products': products,
         'categories': categories,
         'selected_category': category_name,
+        'query': query,
     })
