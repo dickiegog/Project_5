f1950d1a2ca7f41b24bcb9d7f9dffc5021e60103
Richard Goggin <richardgoggin@gmail.com>
2024-12-10 02:33:04 +0000

    Added URLs for products

--- my_pp5/my_PP5/urls.py
+++ my_pp5/my_PP5/urls.py
@@ -21,5 +21,6 @@ urlpatterns = [
      path('admin/', admin.site.urls),
     path('accounts/', include('allauth.urls')), 
      path('profiles/', include('profiles.urls')), 
+     path('products/', include('products.urls')),
     path('', include('home.urls')),
 ]
