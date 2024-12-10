d652c66671319a7ead9d5888557fdd8ee9a6934c
Richard Goggin <richardgoggin@gmail.com>
2024-12-10 22:46:43 +0000

    Added sorting functionality

--- my_pp5/products/views.py
+++ my_pp5/products/views.py
@@ -2,9 +2,10 @@ from django.shortcuts import render
 from .models import Product, Category
 
 def product_catalog(request):
-    """View to display all products with category filtering and search."""
+    """View to display all products with filtering, searching, and sorting."""
     query = request.GET.get('q', None)
     category_name = request.GET.get('category', None)
+    sort_by = request.GET.get('sort', None)
     categories = Category.objects.all()
 
     products = Product.objects.all()
@@ -15,9 +16,16 @@ def product_catalog(request):
     if query:
         products = products.filter(name__icontains=query)
 
+    if sort_by:
+        if sort_by == 'price_asc':
+            products = products.order_by('price')
+        elif sort_by == 'price_desc':
+            products = products.order_by('-price')
+
     return render(request, 'products/catalog.html', {
         'products': products,
         'categories': categories,
         'selected_category': category_name,
         'query': query,
+        'sort_by': sort_by,
     })
