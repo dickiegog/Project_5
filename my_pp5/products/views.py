144d19341f4c5791705b4fa923149333044f72eb
Richard Goggin <richardgoggin@gmail.com>
2024-12-10 02:31:03 +0000

    Created models and view for products

--- my_pp5/products/views.py
+++ my_pp5/products/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from .models import Product
 
-# Create your views here.
+def product_catalog(request):
+    """View to display all products."""
+    products = Product.objects.all()
+    return render(request, 'products/catalog.html', {'products': products})
