f3deca8e66f1e1d3928ff2faf7483ea6488d3478
Richard Goggin <richardgoggin@gmail.com>
2024-12-10 02:03:30 +0000

    Allow option for users to edit their profiles

--- my_pp5/profiles/views.py
+++ my_pp5/profiles/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
+from .models import UserProfile
+from .forms import UserProfileForm
 
-# Create your views here.
+@login_required
+def edit_profile(request):
+    """Allow logged-in users to edit their profile."""
+    profile = UserProfile.objects.get(user=request.user)
+    if request.method == "POST":
+        form = UserProfileForm(request.POST, request.FILES, instance=profile)
+        if form.is_valid():
+            form.save()
+            return redirect("edit_profile")
+    else:
+        form = UserProfileForm(instance=profile)
+    context = {"form": form}
+    return render(request, "profiles/edit_profile.html", context)
