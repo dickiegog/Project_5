96be3c735d13f15009b4e8b1f95dcae74757b8eb
Richard Goggin <richardgoggin@gmail.com>
2024-12-10 22:39:41 +0000

    Added filtering for products

--- my_pp5/products/views.py
+++ my_pp5/products/views.py
@@ -1,7 +1,18 @@
 from django.shortcuts import render
-from .models import Product
+from .models import Product, Category
 
 def product_catalog(request):
-    """View to display all products."""
-    products = Product.objects.all()
-    return render(request, 'products/catalog.html', {'products': products})
+    """View to display all products with category filtering."""
+    category_name = request.GET.get('category', None)
+    categories = Category.objects.all()
+
+    if category_name:
+        products = Product.objects.filter(category__name=category_name)
+    else:
+        products = Product.objects.all()
+
+    return render(request, 'products/catalog.html', {
+        'products': products,
+        'categories': categories,
+        'selected_category': category_name,
+    })
