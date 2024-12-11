a6c49e184c8672b5a9bf5d836ef3b68f12198ef4
Richard Goggin <richardgoggin@gmail.com>
2024-12-11 02:02:21 +0000

    Fixed image not showing on products

--- my_pp5/my_PP5/settings.py
+++ my_pp5/my_PP5/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/3.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -31,6 +31,8 @@ CSRF_TRUSTED_ORIGINS = [
     'https://8000-dickiegog-project5-ljqxmw7dt1f.ws-eu117.gitpod.io',
 ]
 
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 
 # Application definition
