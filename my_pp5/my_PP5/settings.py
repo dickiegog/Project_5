fd075365898b724ea6d965554853559bfe42c126
Richard Goggin <richardgoggin@gmail.com>
2024-12-18 01:26:14 +0000

    resolved profil eimages ot displaying

--- my_pp5/my_PP5/settings.py
+++ my_pp5/my_PP5/settings.py
@@ -81,6 +81,7 @@ TEMPLATES = [
                 'django.template.context_processors.request', #required byAllAuth
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'django.template.context_processors.media', 
             ],
         },
     },
