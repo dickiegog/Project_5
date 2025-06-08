5616b55363ac08a223bef23b9b81d3cf327e03b4
Richard <richardgoggin@gmail.com>
2025-06-09 00:49:35 +0100

    Add test for newlsetter signup

--- newsletter/tests.py
+++ newsletter/tests.py
@@ -1,3 +1,13 @@
 from django.test import TestCase
+from django.urls import reverse
+from .models import Newsletter
 
-# Create your tests here.
+class NewsletterSignupTest(TestCase):
+    def test_newsletter_signup(self):
+        response = self.client.post(reverse('newsletter_signup'), {
+            'email': 'test@example.com'
+        })
+        # Check that the user is redirected (to home)
+        self.assertEqual(response.status_code, 302)
+        # Check that the email was saved
+        self.assertTrue(Newsletter.objects.filter(email='test@example.com').exists())
